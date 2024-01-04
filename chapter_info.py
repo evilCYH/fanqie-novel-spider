e59d364ac2aa726d8733b7b15067175009f188b8
evilCYH <1339138196@qq.com>
2024-01-05 01:26:53 +0800

    change headers

--- chapter_info.py
+++ chapter_info.py
@@ -1,13 +1,39 @@
+import random
+import sys
 import requests
 import re
 import os
 import csv
 import json
 from bs4 import BeautifulSoup
+from time import sleep
 
+def make_random_headers():
+    # 获取打包后可执行文件的路径
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(base_path, "a.txt")
+    with open(file_path, 'r') as file:
+        user_agents = file.readlines()
+    ua = random.choice(user_agents).strip()
+    header = {
+        'User-Agent': ua,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'TE': 'trailers'
+    }
+    return header
 
-def get_novel_info(url, headers):
+def get_novel_info(url):
+    headers = make_random_headers()
     response = requests.get(url, headers=headers)
+    # response.encoding = 'gbk'
 
     if response.status_code != 200:
         raise ValueError(f"网址未能访问，error code：{response.status_code}")
@@ -28,19 +54,25 @@ def get_novel_info(url, headers):
     else:
         raise ValueError("No title match found")
 
-    # 创建相关文件夹
+    # 相关文件夹
     global save_path
-    save_path = f'{os.getcwd()}\download\{novel_name}'
-    show_path = f'./download/{novel_name}/'
-    if os.path.exists(save_path):
-        print(f'文件夹之前已创建, go on')
-    else:
-        print(f'{show_path}  未创建, 开始build')
+
+    # 检查download文件夹
+    download_path = f'./download/'
+    if not os.path.exists(download_path):
+        print("download 文件夹未创建，开始创建")
+        os.mkdir(download_path)
+        print("download 文件夹已创建完毕")
+
+    # 检查小说文件夹
+    save_path = f'./download/{novel_name}'
+    if not os.path.exists(save_path):
+        print(f'{save_path} 文件夹未创建，开始创建')
         os.mkdir(save_path)
         print('文件夹已创建完毕')
 
     # 获取小说info的json
-    novel_info = soup.find('script', type="application/ld+json").text
+    novel_info = soup.find('script', type="application/ld+json").string
     parsed_info = json.loads(novel_info)
 
     # 获取img封面的url
@@ -50,10 +82,10 @@ def get_novel_info(url, headers):
     img = img_response.content
 
     # 保存图像到本地文件
-    if os.path.exists(f'{save_path}\cover.jpg'):
+    if os.path.exists(f'{save_path}/cover.jpg'):
         print('封面已存在, go on')
     else:
-        with open(f"{save_path}\cover.jpg", "wb") as f:
+        with open(f"{save_path}/cover.jpg", "wb") as f:
             f.write(img)
             print('封面已保存完毕')
 
@@ -69,18 +101,18 @@ def get_novel_info(url, headers):
         chapter_list.append([chapter_name, chapter_href, chapter_api])
 
     # 写入CSV文件
-    csv_filename = f'{save_path}\chapter_data.csv'
+    csv_filename = f'{save_path}/chapter_data.csv'
 
     with open(csv_filename, 'w', newline='', encoding="utf_8_sig") as csv_file:
         csv_writer = csv.writer(csv_file)
-
         # 写入表头
         csv_writer.writerow(['Title', 'URL', 'Api'])
-
         # 写入数据
         csv_writer.writerows(chapter_list)
 
-    print(f'Chapter info has been written to {show_path}chapter_data.csv')
+    print(f'章节数据已经写入 {save_path}/chapter_data.csv')
+
+    return save_path
 
 # # test
 # if __name__ == '__main__':
